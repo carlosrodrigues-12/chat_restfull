880785e03481a1eada11a3025bc56bfb0f033ae9
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-21 18:17:52 -0300

    Var

--- python/restFul.py
+++ python/restFul.py
@@ -4,16 +4,16 @@ from markupsafe import escape
 app = Flask(__name__)
 
 @app.route('/sendMessage/<dest>/<msg>')
-def send_message():
+def send_message(dest,msg):
     return f'New Message: Destinatario: {escape(dest)} Message: {escape(msg)}'
 
 @app.route('/user/<username>')
 def show_user_profile(username):
-        return f'User {escape(username)} - Name = {escape(name)}'
+    return f'User {escape(username)}'
 
 @app.route("/")
 def index():
-        return "<p>Index Page<p>"
+    return "<p>Index Page<p>"
 
 @app.route("/hello")
 def hello_world():
