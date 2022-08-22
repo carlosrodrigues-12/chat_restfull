04a23e84e811c9060edfd92d0c7e3a66a04284b0
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-21 21:36:01 -0300

    Id

--- python/flask.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-import os
-from datetime import datetime
-
-app = Flask(__name__)
-app.secret_key = os.getenv('SECRET','randomstring123')
