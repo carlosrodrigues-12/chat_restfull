0ed48196089285b5c130638d30f4dd4c64b942d2
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2022-08-21 22:02:50 -0300

    Chat

--- rest/chat.py
+++ rest/chat.py
@@ -1,5 +1,29 @@
 import uuid
+import os
+from flask import Flask, request, session, jsonify
+from datetime import datetime
+
+id_msg = 0
+
+app = Flask(__name__)
+
+messages = []
 
 myuuid = uuid.uuid4()
 
-print('Your UUID is: ' + str(myuuid))
+def add_message(remt,dest,msg):
+    moment = datetime.now().strftime("%H:%M:%S")
+    messages.append({"timestamp": moment, "remt": remt, "dest": dest, "message": msg, "id_msg": id_msg+1})
+
+@app.route('/sendMessage/<remt>/<dest>/<msg>', methods=['POST'])
+def send_message(remt,dest,msg):
+    add_message(remt,dest,msg)
+    
+    return jsonify(messages)
+
+@app.route("/getMessages/<username>", methods=['GET','POST'])
+def getMessages(username):
+    msgs = [ msg for msg in messages if (msg['dest'] == username) ]
+    return jsonify({'msg':msgs})
+
+app.run(host=os.getenv('IP','0.0.0.0'),port=int(os.getenv('PORT','5000')),debug=True)
